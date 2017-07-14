83a54961845be0c9f7fcd066ad41fce3fedd5d57
ankitrcc89 <ankitrcc89@gmail.com>
2017-07-14 16:12:18 +0530

    Adding Intents

--- src/main/java/sample/HelloWorldUsingWebService.java
+++ src/main/java/sample/HelloWorldUsingWebService.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -8,6 +9,9 @@ import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
 
 public class HelloWorldUsingWebService implements Speechlet {
 
@@ -17,14 +21,58 @@ public class HelloWorldUsingWebService implements Speechlet {
     }
 
     public SpeechletResponse onLaunch(final LaunchRequest launchRequest, final Session session) throws SpeechletException {
-        return null;
+        session.getSessionId();
+        return getWelcomeResponse();
     }
 
     public SpeechletResponse onIntent(final IntentRequest intentRequest, final Session session) throws SpeechletException {
-        return null;
+        Intent intent = intentRequest.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        if ("HelloWorldIntent".equals(intentName)) {
+            return getHelloResponse();
+        } else if ("AMAZON.HelpIntent".equals(intentName)) {
+            return null;
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
     }
 
     public void onSessionEnded(final SessionEndedRequest sessionEndedRequest, final Session session) throws SpeechletException {
 
+    }
+
+    private SpeechletResponse getWelcomeResponse() {
+        String speechText = "Welcome to the Alexa Skills Kit using https web service, you can say hello";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+    private SpeechletResponse getHelloResponse() {
+        String speechText = "Hello world using Web Service";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        return SpeechletResponse.newTellResponse(speech, card);
     }
 }
