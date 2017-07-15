9359df9d859b48d5d1a71c11074336b6677f65fa
ankitrcc89 <ankitrcc89@gmail.com>
2017-07-15 18:04:48 +0530

    Deleted unused file

--- src/main/java/Test.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by agarwank on 7/14/17.
- */
-public class Test {
-
-}
--- src/main/java/helloworld/HelloWorldServlet.java
+++ src/main/java/helloworld/HelloWorldServlet.java
@@ -2,9 +2,9 @@ package helloworld;
 
 import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 
-public class HelloWorldServlet extends SpeechletServlet{
+public class HelloWorldServlet extends SpeechletServlet {
+
     public HelloWorldServlet() {
-        System.out.println("Inside hello world Handler");
         this.setSpeechlet(new HelloWorldSpeechlet());
     }
 }
